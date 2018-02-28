803cb5fd21b3872895dc29d1d9f3a22b62c3d065
josephcomer <joseph.comer@ncf.edu>
2018-02-28 16:50:52 -0500

    Made annotations.

--- app.py
+++ app.py
@@ -12,7 +12,12 @@ df = pd.read_csv(
 # Create a Dash object instance
 app = dash.Dash()
 
-
+# The layout attribute of the Dash object, app
+# is where you include the elements you want to appear in the
+# dashboard. Here, dcc.Graph and dcc.Slider are separate
+# graph objects. Most of Graph's features are defined
+# inside the function update_figure, but we set the id
+# here so we can reference it inside update_figure
 app.layout = html.Div([
     dcc.Graph(id='graph-with-slider'),
     dcc.Slider(
@@ -26,18 +31,33 @@ app.layout = html.Div([
 ])
 
 
+# Notice the Input and Outputs in this wrapper correspond to
+# the ids of the components in app.layout above.
 @app.callback(
     dash.dependencies.Output('graph-with-slider', 'figure'),
     [dash.dependencies.Input('year-slider', 'value')])
 def update_figure(selected_year):
+    """Define how the graph is to be updated based on the slider."""
+
+    # Depending on the year selected on the slider, filter the db
+    # by that year.
     filtered_df = df[df.year == selected_year]
+
+    # The go.Scatter graph object go.Scatter contains information
+    # about points to put on a scatter plot. Here, we create one
+    # Scatter object for each continent by filtering, and append each
+    # Scatter object to a list. The whole list of Scatterplots will
+    # appear on one graph--'graph-with-slider'
     traces = []
     for i in filtered_df.continent.unique():
         df_by_continent = filtered_df[filtered_df['continent'] == i]
-        traces.append(go.Scatter(
-            x=df_by_continent['gdpPercap'],
-            y=df_by_continent['lifeExp'],
-            text=df_by_continent['country'],
+        """The mode controls the appearance of the points of data. Try changing
+        mode below to 'lines' and see the change. A complete list of modes is
+        available at https://plot.ly/python/reference/#scatter"""
+        traces.append(go.Scatter(  # Scatter is just one plotly.graph_obj (.go)
+            x=df_by_continent['gdpPercap'],   # graph type. Try changing
+            y=df_by_continent['lifeExp'],     # to go.Scatter3d.
+            text=df_by_continent['country'],  # (It won't look great, here.)
             mode='markers',
             opacity=0.7,
             marker={
